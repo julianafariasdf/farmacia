dcf60b7aecfeb29e4cf19f9e74b078dc3112710d
Juliana Ferreira <juliana.ferreira.farias@gmail.com>
2021-06-02 18:22:00 -0300

    Projeto Criado

---
+++ src/main/java/br/com/teste/Farmacia/model/Produto.java
@@ -0,0 +1,5 @@
+package br.com.teste.Farmacia.model;
+
+public class Produto {
+
+}
---
+++ src/test/java/br/com/teste/Farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.Farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
